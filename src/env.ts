c91beb27afabcc118b730039f337ccb14cfb816f
Leonardo Gurgel <leo@gurgel.io>
2024-12-01 16:21:19 -0300

    eject fastify

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,12 @@
+import { configDotenv } from "dotenv";
 import { z } from "zod";
+
+configDotenv();
 
 const envSchema = z.object({
   DATABASE_URL: z.string().default("file:./dev.db"),
   JWT_SECRET: z.string(),
+  ENV: z.enum(["development", "test", "production"]).default("development"),
 });
 
 export const env = envSchema.parse(process.env);
