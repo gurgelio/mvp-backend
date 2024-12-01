9cb313d94c9d8bf67f7cd4710a4e91e0166acd72
Leonardo Gurgel <leo@gurgel.io>
2024-12-01 16:21:19 -0300

    eject fastify

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,73 @@
-import AutoLoad, { type AutoloadPluginOptions } from "@fastify/autoload";
+import AutoLoad from "@fastify/autoload";
 import fastifySensible from "@fastify/sensible";
-import type { FastifyPluginCallback } from "fastify";
+import closeWithGrace from "close-with-grace";
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import { ZodError } from "zod";
+import { env } from "./env.js";
 import { prisma } from "./lib/prisma.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export type AppOptions = {
-  // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
 
-// Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {};
+const app = createServer();
 
-const app: FastifyPluginCallback<AppOptions> = (fastify, opts) => {
-  fastify.register(fastifySensible);
+closeWithGrace({ delay: 500 }, async function ({ err }) {
+  if (err) app.log.error(err);
+
+  await app.close();
+});
+
+app.listen({ port: 3000 }).catch((err: unknown) => {
+  app.log.error(err);
+  process.exit(1);
+});
+
+export function createServer() {
+  const app = fastify({
+    logger: envToLogger[env.ENV],
+  });
+
+  app.register(fastifySensible);
+  app.setValidatorCompiler(validatorCompiler);
+  app.setSerializerCompiler(serializerCompiler);
 
   // This loads all plugins defined in routes
   // define your routes in one of these
-  void fastify.register(AutoLoad, {
+  void app.register(AutoLoad, {
     dir: path.join(__dirname, "routes"),
-    options: opts,
     forceESM: true,
   });
 
-  fastify.addHook("onClose", async () => {
+  app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) return reply.notFound();
+  });
+
+  app.addHook("onClose", async () => {
     await prisma.$disconnect();
   });
-};
 
-export default app;
-export { app, options };
+  return app.withTypeProvider<ZodTypeProvider>();
+}
+
+export type App = ReturnType<typeof createServer>;
