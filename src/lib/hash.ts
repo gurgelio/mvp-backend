6609bd644230d2ef1461a59c908d91862436e243
Leonardo Gurgel <leo@gurgel.io>
2024-12-02 18:45:11 -0300

    add auth

--- src/lib/hash.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from "crypto";
-
-export function digest(password: string) {
-  return createHash("sha256").update(password).digest("base64");
-}
--- src/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyPluginCallback } from "fastify";
-
-const root: FastifyPluginCallback = (fastify) => {
-  fastify.get("/", () => ({ root: true }));
-};
-
-export default root;
