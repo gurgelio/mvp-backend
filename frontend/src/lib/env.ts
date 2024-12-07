91339579cd1a78afd46cabd97aff8e001abdf140
Leonardo Gurgel <leo@gurgel.io>
2024-12-06 21:39:36 -0300

    add default api url

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  VITE_API_URL: z.string(),
+  VITE_API_URL: z.string().default("http://localhost:3000"),
   MODE: z.enum(["production", "development", "test"]).default("development"),
 });
 
